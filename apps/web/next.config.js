822ab52f5b362ba1c05ce1fa1e7fa71245f66836
astabidasaaa <astabidasabcd@gmail.com>
2024-08-29 20:34:49 +0700

    feat(FP04-17): add basic utility and config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    PORT: '3000',
+    THUMBNAIL_API_URL: 'http://localhost:8000/thumbnail',
+    API_URL: 'http://localhost:8000/api',
+    BASE_URL: 'http://localhost:3000',
+    ACCESS_TOKEN_SECRET: '1234567890qwertyuiop',
+    REFRESH_TOKEN_SECRET: 'asdfghjklzxcvbnm',
+    EMAIL_VERIFICATION_SECRET: '0987654321lkjhgfdsa',
+  },
+  images: {
+    formats: ['image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
