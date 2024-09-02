86942157ef2ebd09a996da358c57ba480b382bc9
citakurnia <citakurniah@gmail.com>
2024-09-02 16:09:53 +0700

    feat(FP04-22): dashboard layout (header and sidebar)

--- apps/web/src/app/(dashboard)/dashboard/category/add-subcategory/page.tsx
+++ apps/web/src/app/(dashboard)/dashboard/category/add-subcategory/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 type Props = {};
 
-export default function DashboardPage(props: Props) {
+export default function AddSubcategoryPage(props: Props) {
   return (
     <>
       <DashboardHomeView />
---
+++ apps/web/src/app/(dashboard)/dashboard/product/brand/page.tsx
@@ -0,0 +1,8 @@
+// import BrandView from '@/view/dashboard/brand';
+import React from 'react';
+
+type Props = {};
+
+export default function BrandPage(props: Props) {
+  return <>{/* <BrandView /> */}</>;
+}
