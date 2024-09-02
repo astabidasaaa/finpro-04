e616d20f5745a187ca8ff155a1ecd222024745e3
astabidasaaa <astabidasabcd@gmail.com>
2024-09-02 23:19:27 +0700

    feat(FP04-16): add temporary layout to (main) and /pengaturan

--- apps/web/src/app/(main)/pengaturan/layout.tsx
+++ apps/web/src/app/(main)/pengaturan/layout.tsx
@@ -25,23 +25,21 @@ const sidebarNavItems = [
 
 const SettingsLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
-      <div className="space-y-6 p-4 md:px-12 lg:px-24 pb-16 block">
-        <div className="space-y-0.5">
-          <h2 className="text-2xl font-bold tracking-tight">Pengaturan</h2>
-          <p className="text-muted-foreground">
-            Kelola pengaturan akun Anda dan atur preferensi tampilan.
-          </p>
-        </div>
-        <Separator className="my-6" />
-        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0 w-full">
-          <aside className="lg:-mx-4 w-full lg:w-1/5">
-            <SidebarNav items={sidebarNavItems} />
-          </aside>
-          <div className="flex-1 ">{children}</div>
-        </div>
+    <div className="w-full px-4 md:px-12 lg:px-24 py-8 space-y-6">
+      <div className="space-y-0.5">
+        <h2 className="text-2xl font-bold tracking-tight">Pengaturan</h2>
+        <p className="text-muted-foreground">
+          Kelola pengaturan akun Anda dan atur preferensi tampilan.
+        </p>
       </div>
-    </>
+      <Separator className="my-6" />
+      <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0 w-full">
+        <aside className="lg:-mx-4 w-full lg:w-1/5">
+          <SidebarNav items={sidebarNavItems} />
+        </aside>
+        <div className="flex-1 ">{children}</div>
+      </div>
+    </div>
   );
 };
 
