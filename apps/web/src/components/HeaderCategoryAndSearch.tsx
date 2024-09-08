3f6b112af601f49fe37a42556a5274e7e5a6316b
citakurnia <citakurniah@gmail.com>
2024-09-09 04:56:36 +0700

    feat(FP04-22): add search action in header

--- apps/web/src/components/HeaderCategoryAndSearch.tsx
+++ apps/web/src/components/HeaderCategoryAndSearch.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { Input } from './ui/input';
 import { Search } from 'lucide-react';
 import HeaderCategoryBtn from './HeaderCategoryBtn';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 const HeaderCategoryAndSearch = () => {
+  const [text, setText] = useState('');
+  const router = useRouter();
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      router.push(`/search?keyword=${text}`);
+    }
+  };
+
   return (
     <>
       <HeaderCategoryBtn />
@@ -11,6 +22,8 @@ const HeaderCategoryAndSearch = () => {
         <Input
           type="search"
           placeholder="Search..."
+          onChange={(e) => setText(e.target.value)}
+          onKeyDown={handleKeyDown}
           className="w-4/5 rounded-full bg-background pl-8 md:w-[200px] lg:w-[336px] h-8 md:h-10"
         />
         <Search className="absolute left-2.5 top-2 md:top-3 h-4 w-4 text-muted-foreground" />
