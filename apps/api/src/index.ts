649da22d1007a7bae6f90d8a2008e371dfd24e59
citakurnia <citakurniah@gmail.com>
2024-09-12 23:27:04 +0700

    fix(FP04-20): delete double category route after merging

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -8,14 +8,12 @@ import { ShippingRouter } from './routers/shippingRouter';
 import { GetOrderRouter } from './routers/getOrderRouter';
 import { StoreRouter } from './routers/storeRouter';
 import { PromotionRouter } from './routers/promotionRouter';
-import { CategoryRouter } from './routers/categoryRoute';
 import { CourierRouter } from './routers/courierRouter';
 import { SubcategoryRouter } from './routers/subcategoryRouter';
 import { CategoryRouter } from './routers/categoryRouter';
 import { BrandRouter } from './routers/brandRouter';
 import { ProductRouter } from './routers/productRouter';
 import { AdminRouter } from './routers/adminRouter';
-
 
 function main() {
   // Initialize the app with all the routes
