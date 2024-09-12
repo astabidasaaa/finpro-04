a3642aff9a3dfece66edf33827b69ca11283fb67
citakurnia <citakurniah@gmail.com>
2024-09-12 15:58:50 +0700

    fix(FP04-20): make function call in validateUserRole

--- apps/api/src/middlewares/adminValidator.ts
+++ apps/api/src/middlewares/adminValidator.ts
@@ -43,7 +43,7 @@ export const validateAdminCreation = [
   body('role').trim().notEmpty().withMessage('Role wajib diisi'),
   body('storeId').optional().isNumeric(),
 
-  validateUserRole,
+  validateUserRole(),
 
   (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
@@ -70,8 +70,42 @@ export const validateAdminUpdate = [
     .optional()
     .isLength({ max: 48 })
     .withMessage('Nama berisi maksimal 48 karakter'),
-  body('role').trim().optional().withMessage('Role wajib diisi'),
+  body('role').trim().optional(),
   body('storeId').optional().isNumeric(),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
+    }
+
+    next();
+  },
+];
+
+export const validateChangeAdminPassword = [
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email wajib diisi')
+    .isEmail()
+    .withMessage('Format email tidak sesuai')
+    .isLength({ min: 3, max: 48 })
+    .withMessage('Email harus berisi antara 3 hingga 48 karakter')
+    .toLowerCase(),
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('Password wajib diisi')
+    .isLength({ min: 8 })
+    .withMessage('Password harus berisi lebih dari 8 karakter'),
+  body('newPassword')
+    .trim()
+    .notEmpty()
+    .withMessage('Password wajib diisi')
+    .isLength({ min: 8 })
+    .withMessage('Password harus berisi lebih dari 8 karakter'),
 
   (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
