e2c25b7e5ecd559b22ba9da390ba9d95766cd45c
citakurnia <citakurniah@gmail.com>
2024-09-14 02:28:45 +0700

    feat(FP04-25): add inventory navigation list to both store and super admin

--- apps/web/src/components/dashboard/storeAdminMenu.tsx
+++ apps/web/src/components/dashboard/storeAdminMenu.tsx
@@ -39,9 +39,19 @@ export const storeAdminMenuList = [
     ],
   },
   {
-    label: 'Inventori',
+    label: 'Inventaris',
     icon: <Package2 className="size-5" />,
     href: '/dashboard/inventory',
+    child: [
+      {
+        label: 'Inventaris Produk',
+        href: '/dashboard/inventory/product-inventory',
+      },
+      {
+        label: 'Riwayat Inventaris',
+        href: '/dashboard/inventory/inventory-history',
+      },
+    ],
   },
   {
     label: 'Manajemen Diskon',
