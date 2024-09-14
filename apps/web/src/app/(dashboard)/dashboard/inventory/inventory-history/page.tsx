cf89e411dfe857077bfc44f944708f31a3b79aaa
citakurnia <citakurniah@gmail.com>
2024-09-14 18:35:27 +0700

    feat(FP04-25): front-end for inventory updates page

---
+++ apps/web/src/app/(dashboard)/dashboard/inventory/inventory-history/page.tsx
@@ -0,0 +1,12 @@
+import InventoryHistoryView from '@/view/dashboard/inventoryHistory';
+import React from 'react';
+
+type Props = {};
+
+export default function InventoryHistoryPage(props: Props) {
+  return (
+    <>
+      <InventoryHistoryView />
+    </>
+  );
+}
--- apps/web/src/types/inventoryType.ts
+++ apps/web/src/types/inventoryType.ts
@@ -28,3 +28,43 @@ export enum UpdateDetail {
   EXPIRATION = 'Kadaluarsa',
   CANCELLED_ORDER = 'Order dibatalkan',
 }
+
+export enum InventoryUpdateType {
+  ADD = 'ADD',
+  REMOVE = 'REMOVE',
+}
+
+export enum InventoryUpdateDetail {
+  STOCK_IN = 'STOCK_IN',
+  STOCK_OUT = 'STOCK_OUT',
+  ADJUSTMENT = 'ADJUSTMENT',
+  DAMAGED = 'DAMAGED',
+  EXPIRATION = 'EXPIRATION',
+  CANCELLED_ORDER = 'CANCELLED_ORDER',
+}
+
+export enum SortTime {
+  TIMEDESC = 'timeDesc',
+  TIMEASC = 'timeAsc',
+}
+
+export type InventoryUpdateProps = {
+  id: number;
+  creatorId: number;
+  inventoryId: number;
+  type: InventoryUpdateType;
+  detail: InventoryUpdateDetail;
+  description: string | null;
+  stockChange: number;
+  createdAt: Date;
+  inventory: {
+    product: {
+      id: number;
+      name: string;
+    };
+    store: {
+      id: number;
+      name: string;
+    };
+  };
+};
