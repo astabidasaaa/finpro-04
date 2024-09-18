b2f3895da14a60c26af80ab2955f6ca4ef662932
citakurnia <citakurniah@gmail.com>
2024-09-19 00:07:20 +0700

    fix(FP04-23): typo in discount product page

--- apps/web/src/view/dashboard/(promotion)/discountProduct/index.tsx
+++ apps/web/src/view/dashboard/(promotion)/discountProduct/index.tsx
@@ -153,7 +153,7 @@ export default function DiscountProductPromotionView() {
     <div className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
       <div className="flex items-center">
         <h1 className="text-lg font-semibold md:text-2xl">
-          Promosi Gratis Produk
+          Promosi Diskon Produk
         </h1>
       </div>
       <div className="w-full">
