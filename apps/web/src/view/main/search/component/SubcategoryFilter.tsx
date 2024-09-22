71cf8e36d2b9d8c4f2ba92b7537f12a6e4bb9aa1
astabidasaaa <astabidasabcd@gmail.com>
2024-09-23 04:24:43 +0700

    fix: make a difference between brand and category for id and htmlFor

--- apps/web/src/view/main/search/component/SubcategoryFilter.tsx
+++ apps/web/src/view/main/search/component/SubcategoryFilter.tsx
@@ -41,10 +41,10 @@ export default function SubcategoryFilter({
               >
                 <RadioGroupItem
                   value="0"
-                  id="0"
+                  id="category-0"
                   className="!min-w-3 w-3 !h-3"
                 />
-                <Label htmlFor="0">
+                <Label htmlFor="category-0">
                   <span className="font-normal text-[13px] line-clamp-2">
                     Semua kategori
                   </span>
@@ -52,15 +52,15 @@ export default function SubcategoryFilter({
               </div>
               {subcategories.map((category) => (
                 <div
-                  key={category.id}
+                  key={`category-${category.id.toString()}`}
                   className="flex justify-start items-center py-0.5 text-muted-foreground gap-2"
                 >
                   <RadioGroupItem
                     value={category.id.toString()}
-                    id={category.id.toString()}
+                    id={`category-${category.id.toString()}`}
                     className="!min-w-3 w-3 !h-3"
                   />
-                  <Label htmlFor={category.id.toString()}>
+                  <Label htmlFor={`category-${category.id.toString()}`}>
                     <span className="font-normal text-[13px] line-clamp-2">
                       {category.name}
                     </span>
