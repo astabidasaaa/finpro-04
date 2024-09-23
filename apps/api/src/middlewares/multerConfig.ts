4e73b35126f0cf84636621c80e21a2b9e2d6d854
astabidasaaa <astabidasabcd@gmail.com>
2024-09-23 19:40:06 +0700

    fix: fix payment upload bug on production

--- apps/api/src/middlewares/multerConfig.ts
+++ apps/api/src/middlewares/multerConfig.ts
@@ -4,12 +4,12 @@ import path from 'path';
 // Multer configuration
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '..', 'uploads', 'payment'));
+    cb(null, path.join(__dirname, '../../public', 'payment'));
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + '-' + file.originalname);
-  }
+  },
 });
 
 const fileFilter = (req: any, file: any, cb: any) => {
@@ -20,7 +20,11 @@ const fileFilter = (req: any, file: any, cb: any) => {
   if (extname && mimetype) {
     cb(null, true);
   } else {
-    cb(new Error('File type not supported. Only JPEG, JPG, and PNG files are allowed.'));
+    cb(
+      new Error(
+        'File type not supported. Only JPEG, JPG, and PNG files are allowed.',
+      ),
+    );
   }
 };
 
